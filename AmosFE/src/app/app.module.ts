5ff65942997eb97b0db08f5eec1ea70c41c44d15
patidarharsh <harsh.patidar99@gmail.com>
2020-02-29 15:00:18 +0530

    Work till 29Feb 3:00PM

--- AmosFE/src/app/app.module.ts
+++ AmosFE/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MDBBootstrapModule, NavbarModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NavbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
