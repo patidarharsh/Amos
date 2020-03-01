f0a46af0daec7de3e2527271430b9d3ec00507b9
patidarharsh <harsh.patidar99@gmail.com>
2020-03-01 11:31:15 +0530

    1March 11:31AM

--- AmosFE/src/app/app.module.ts
+++ AmosFE/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     MDBBootstrapModule.forRoot(),
